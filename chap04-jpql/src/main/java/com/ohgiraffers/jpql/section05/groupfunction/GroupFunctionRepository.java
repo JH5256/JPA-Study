80e0cfe82975a5a3698440e22d39b4f395027e20
JH5256 <skdltmvhs2@naver.com>
2024-05-13 10:15:16 +0900

    commit

---
+++ chap04-jpql/src/main/java/com/ohgiraffers/jpql/section05/groupfunction/GroupFunctionRepository.java
@@ -0,0 +1,14 @@
+package com.ohgiraffers.jpql.section05.groupfunction;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+public class GroupFunctionRepository {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    public long countMenuOfCategory(int categoryCode) {
+        String jpql = "SELECT COUNT(m.menuCode) FROM Section05Menu m WHERE m.categoryCode = :categoryCode";
+    }
+}
